0f482916c245e2842dd4b499dcae473bf7f41632
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-13 16:26:40 -0500

    completed js fetch

--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 const url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
 
-const key = '?key=b2eaff5c-5497-49e8-8484-9745a09a2b2a'
+const word = document.getElementById("Word")
 
-const header = document.getElementById("Header")
+const definition = document.getElementById("DefitnitionText")
 
-const random = document.getElementById("Random")
-
-const day = document.getElementById("Day")
-
-const definition = document.getElementById("Definition")
-
-const history = document.getElementById("History")
-
-const saved = document.getElementById("Saved")
-
-const submitButton = document.getElementById("Submit")
+const pronunciation = document.getElementById("Pronunciation")
 
 const input = document.getElementById("Input")
 
-submitButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    word = input.value
-    console.log(word)
+const search = document.getElementById("Search")
 
-    fetch(`${url}${word}${key}`)
+let word1 = ''
+
+search.addEventListener('submit', (e) => {
+    e.preventDefault()
+    word1 = input.value
+    const wordTag = document.createElement('p')
+    wordTag.innerText = word1
+    word.appendChild(wordTag)
+
+    // console.log(word1)
+
+    fetch(`${url}${word1}${key}`)
         .then(res => res.json())
         .then((data) => {
-            console.log(data)
+            definitions = data[0].shortdef
+            pronounced = data[0].hwi.hw
+            renderPronunciation(pronounced)
+            definitions.forEach(element => {
+                renderDefinition(element)
+            });
         })
 })
 
-let word = ''
+function renderDefinition(newDef) {
+    const pTag = document.createElement('p')
+    pTag.innerText = newDef
+    definition.appendChild(pTag)
+}
+
+function renderPronunciation(newPro) {
+    const pTag = document.createElement('p')
+    pTag.innerText = newPro
+    pronunciation.appendChild(pTag)
+}
