5ad63b572acd9f6d13a4e6805ccf927dd2bd4d36
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-13 14:07:31 -0500

    added keys

---
+++ index.js
@@ -0,0 +1,12 @@
+const header = document.getElementById("Header")
+
+const random = document.getElementById("Random")
+
+const day = document.getElementById("Day")
+
+const definition = document.getElementById("Definition")
+
+const history = document.getElementById("History")
+
+const saved = document.getElementById("Saved")
+
