d355422e5b3702a7936ce67bea43d1fdab406b56
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-14 01:16:23 -0500

    added saved word event listner

--- index.js
+++ index.js
@@ -14,17 +14,31 @@ const search = document.getElementById("Search")
 
 const history = document.getElementById("history-list")
 
+const saved = document.getElementById("SavedWord")
+
+const savedUl = document.getElementById("salved-list")
+
+
+
+// Search bar submit event listner
 search.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    // Resets the Word: section
     wordElement.innerHTML = ''
-    word1 = input.value
-    fullUrl = `${url}${word1}${key}`
-    let correctCase = word1.charAt(0).toUpperCase() + word1.slice(1);
+
+    // Defines the searched word
+    let word1 = input.value
+
     // uppercase the first letter of the word
+    correctCase = word1.charAt(0).toUpperCase() + word1.slice(1);
+
+    // Add searched word to the Word: section
     const wordTag = document.createElement('p')
     wordTag.innerText = correctCase
     wordElement.appendChild(wordTag)
 
+    // Add the searched word to the history section
     const wordHistory = document.createElement('li')
     wordHistory.innerText = correctCase
     wordHistory.addEventListener('click', (e) => {
@@ -34,52 +48,90 @@ search.addEventListener('submit', (e) => {
     })
     history.appendChild(wordHistory)
 
-    // function persistSavedWord(word) {
-    fetch(fullUrl, {
-        method: 'POST',
-        headers: {
-            // something goes in here with the API key to give us access to the API
-            // something like this?
-            // "Authorization": `Api-Key ${fullUrl}`,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify('Textual content')
-    }).then(res => res.json())
-        .then(data => console.log(data))
+    // Full URL for fetch Request
+    fullUrl = `${url}${word1}${key}`
+
+     //Make word history persist
+     // function persistSavedWord(word) {
+    // fetch(fullUrl, {
+    //     method: 'POST',
+    //     headers: {
+    //         // something goes in here with the API key to give us access to the API
+    //         // something like this?
+    //         // "Authorization": `Api-Key ${fullUrl}`,
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify('Textual content')
+    // }).then(res => res.json())
+    //     .then(data => console.log(data))
     // }
 
-    // persistSavedWord("kangaroo")
-    // make word history persist
-
-    // console.log(word1)
-
+    // Fetch request
     fetch(fullUrl)
         .then(res => res.json())
         .then((data) => {
+
+            // Accesses Definitions
             const definitions = data[0].shortdef
+
+            // Accesses Pronuciation
             const pronounced = data[0].hwi.hw
+
+            // Renders pronuciation to the Pronuciation: section
             renderPronunciation(pronounced)
+
+            // Resets the definition
             definition.innerHTML = ''
+
+            // Renders all definitions
             definitions.forEach(element => {
                 renderDefinition(element)
             });
         })
+    // Resets the search bar
     input.value = ''
 })
 
+// Function for rendering Definition to the Definition: Section
 function renderDefinition(newDef) {
     const pTag = document.createElement('p')
     pTag.innerText = newDef
     definition.appendChild(pTag)
 }
 
+// Function for rendering Pronuciation to the Pronuciation: Section
 function renderPronunciation(newPro) {
     pronunciation.innerHTML = ''
     const pTag = document.createElement('p')
     pTag.innerText = newPro
     pronunciation.appendChild(pTag)
 }
+
+saved.addEventListener('click', (e) => {
+    e.preventDefault()
+    const savedWord = document.createElement('li')
+    savedWord.innerText = correctCase
+    savedUl.appendChild(savedWord)
+})
+
+    // function persistSavedWord(word) {
+    // fetch(fullUrl, {
+    //     method: 'POST',
+    //     headers: {
+    //         // something goes in here with the API key to give us access to the API
+    //         // something like this?
+    //         // "Authorization": `Api-Key ${fullUrl}`,
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify('Textual content')
+    // }).then(res => res.json())
+    //     .then(data => console.log(data))
+    // }
+
+    // persistSavedWord("kangaroo")
+ 
 
 // Make the saved words list persist locally by posting them to db.json
 // Start the json server with the following command:
