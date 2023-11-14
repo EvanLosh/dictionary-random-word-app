1870e4d48d1ec20e0bb39d8f983a3d20ad139268
Alex Isaacs <alexisaacs18@gmail.com>
2023-11-14 14:14:24 -0500

    cleaned up code for js file

--- index.js
+++ index.js
@@ -22,12 +22,16 @@ const li = document.getElementById("reference")
 
 const randomButton = document.getElementById("RamdomButtom")
 
+const randomWords = ['Stuff', 'Things', 'Arrived', 'Arrogancy', 'Arrogate', 'Arsenide', 'Arshin', 'Arterio', 'Artful']
+
+const randomIndex = Math.floor(Math.random() * randomWords.length);
+const randomElement = randomWords[randomIndex]
+
+generateUrl(randomElement)
+
 // Search bar submit event listner
 search.addEventListener('submit', (e) => {
     e.preventDefault()
-
-    // Resets the Word: section
-    wordElement.innerHTML = ''
 
     // Defines the searched word
     let word1 = input.value
@@ -35,10 +39,7 @@ search.addEventListener('submit', (e) => {
     // uppercase the first letter of the word
     correctCase = word1.charAt(0).toUpperCase() + word1.slice(1);
 
-    // Add searched word to the Word: section
-    const wordTag = document.createElement('p')
-    wordTag.innerText = correctCase
-    wordElement.appendChild(wordTag)
+    renderWord(correctCase)
 
     // Add the searched word to the history section
     const wordHistory = document.createElement('li')
@@ -46,65 +47,39 @@ search.addEventListener('submit', (e) => {
     // Add event listner to the element
     wordHistory.addEventListener('click', (e) => {
         e.preventDefault()
-        // Create api of the historical word
-        const historyUrl = `${api}${wordHistory.textContent}${key}`
-        // Fetch the historical word data
-        fetch(historyUrl)
-            .then(res => res.json())
-            .then((data) => {
-                // Accesses Historical Definitions
-                const definitions = data[0].shortdef
-
-                // Accesses History Pronuciation
-                const pronounced = data[0].hwi.hw
-
-                // Renders historical pronuciation to the Pronuciation: section
-                renderPronunciation(pronounced)
-
-                // Resets the definition
-                definition.innerHTML = ''
-
-                // Renders all definitions
-                definitions.forEach(element => {
-                    renderDefinition(element)
-                })
-            })
-        renderWord(wordHistory.textContent)
+        generateUrl(wordHistory.textContent)
     })
     //call the function to add the event listener to change the color of a word when you put your mouse over it
-    colorChange(wordHistory) 
+    colorChange(wordHistory)
     // Appends the word to the history section
     history.insertBefore(wordHistory, history.firstChild);
     // history.appendChild(wordHistory)
 
-
-    // URL with searched word for fetch Request
-    fullUrl = `${api}${correctCase}${key}`
-
-    // Fetch request
-    fetch(fullUrl)
-        .then(res => res.json())
-        .then((data) => {
-
-            // Accesses Definitions
-            const definitions = data[0].shortdef
-
-            // Accesses Pronuciation
-            const pronounced = data[0].hwi.hw
-
-            // Renders pronuciation to the Pronuciation: section
-            renderPronunciation(pronounced)
-
-            // Resets the definition
-            definition.innerHTML = ''
-
-            // Renders all definitions
-            definitions.forEach(element => {
-                renderDefinition(element)
-            });
-        })
+    generateUrl(correctCase)
     // Resets the search bar
     input.value = ''
+})
+
+// Add event listner to the saved word button
+saved.addEventListener('click', (e) => {
+    e.preventDefault()
+    // create saved word
+    const savedWord = document.createElement('li')
+    savedWord.innerText = wordElement.children[0].textContent
+    // add event lisner to the element
+    savedWord.addEventListener('click', (e) => {
+        e.preventDefault()
+        generateUrl(savedWord.textContent)
+    })
+    colorChange(savedWord)
+    // Appends the word to the saved word section
+    savedUl.appendChild(savedWord)
+})
+
+randomButton.addEventListener('click', (e) => {
+    const randomIndex = Math.floor(Math.random() * randomWords.length);
+    const randomElement = randomWords[randomIndex]
+    generateUrl(randomElement)
 })
 
 // Function for rendering Definition to the Definition: Section
@@ -131,81 +106,19 @@ function renderWord(newWord) {
 
 function colorChange(element) {
     let defaultColor = element.style.color
-    element.addEventListener("mouseover", (e) => {   
-    e.preventDefault()
-    element.style.color = "green"   
-    })
-    element.addEventListener("mouseout", (e) => {   
+    element.addEventListener("mouseover", (e) => {
         e.preventDefault()
-        element.style.color = defaultColor   
-        })
+        element.style.color = "green"
+    })
+    element.addEventListener("mouseout", (e) => {
+        e.preventDefault()
+        element.style.color = defaultColor
+    })
 }
 
-// Add event listner to the saved word button
-saved.addEventListener('click', (e) => {
-    e.preventDefault()
-    // create saved word
-    const savedWord = document.createElement('li')
-    savedWord.innerText = wordElement.children[0].textContent
-    // add event lisner to the element
-    savedWord.addEventListener('click', (e) => {
-        e.preventDefault()
-        // Create api of the saved word
-        const savedUrl = `${api}${savedWord.textContent}${key}`
-        // Fetch the saved word data
-        fetch(savedUrl)
-            .then(res => res.json())
-            .then((data) => {
-                // Accesses Saved Definitions
-                const definitions = data[0].shortdef
-
-                // Accesses Saved Pronuciation
-                const pronounced = data[0].hwi.hw
-
-                // Renders saved pronuciation to the Pronuciation: section
-                renderPronunciation(pronounced)
-
-                // Resets resets definition
-                definition.innerHTML = ''
-
-                // Renders all definitions
-                definitions.forEach(element => {
-                    renderDefinition(element)
-                })
-            })
-        renderWord(savedWord.textContent)
-    })
-    colorChange(savedWord)
-    // Appends the word to the saved word section
-    savedUl.appendChild(savedWord)
-})
-
-// Make the saved words list persist locally by posting them to db.json
-// Start the json server with the following command:
-// json-server --watch db.json --port 4000
-// This function should be called when the 'save word' button is clicked
-// fetch("http://localhost:4000", {
-//     method: 'POST',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ a: word, b: 'Textual content' })
-// })
-
-// persistSavedWord("kangaroo")
-
-const randomWords = ['Stuff', 'Things', 'Arrived', 'Arrogancy', 'Arrogate', 'Arsenide', 'Arshin', 'Arterio', 'Artful']
-
-const randomIndex = Math.floor(Math.random() * randomWords.length);
-
-const randomElement = randomWords[randomIndex]
-
-const randomUrl = `${api}${randomElement}${key}`
-
-randomButton.addEventListener('click', (e) => {
-    e.preventDefault
-    fetch(randomUrl)
+function generateUrl(theWord) {
+    let url = `${api}${theWord}${key}`
+    fetch(url)
         .then(res => res.json())
         .then(data => {
             // Accesses Saved Definitions
@@ -225,5 +138,20 @@ randomButton.addEventListener('click', (e) => {
                 renderDefinition(element)
             })
         })
-    renderWord(randomElement)
-})
+    renderWord(theWord)
+}
+
+// Make the saved words list persist locally by posting them to db.json
+// Start the json server with the following command:
+// json-server --watch db.json --port 4000
+// This function should be called when the 'save word' button is clicked
+// fetch("http://localhost:4000", {
+//     method: 'POST',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ a: word, b: 'Textual content' })
+// })
+
+// persistSavedWord("kangaroo")
