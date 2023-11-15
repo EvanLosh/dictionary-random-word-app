3ff3946cd80d41f41f9092afdad936ba1911adc1
Evan Losh <evand.losh@gmail.com>
2023-11-14 20:15:48 -0500

    commented, refactored, ready to deliver

--- index.js
+++ index.js
@@ -1,48 +1,37 @@
+
+// API
 const api = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
-
-const history = document.getElementById("history-list")
-
-const historyUrl = "http://localhost:3000/wordhistory"
-
-const savedWordsUrl = "http://localhost:3000/savedwords"
-
-const wordElement = document.getElementById("WordLocation")
-
 const key = '?key=b2eaff5c-5497-49e8-8484-9745a09a2b2a'
-
+// start json-server WITHOUT the watch option
+const historyUrl = "http://localhost:3000/wordhistory"
+const savedWordsUrl = "http://localhost:3000/savedwords"
+// grab DOM elements
+const history = document.getElementById("history-list")
+const wordElement = document.getElementById("WordLocation")
 const definition = document.getElementById("DefLocation")
-
 const pronunciation = document.getElementById("ProLocation")
-
 const input = document.getElementById("Input")
-
 const search = document.getElementById("Search")
-
 const saveButton = document.getElementById("save-this-word")
-
 const savedUl = document.getElementById("saved-list")
-
 const randomButton = document.getElementById("RamdomButtom")
-
 const errorMessage = document.getElementById("error-message")
-
 const historyTitle = document.getElementById("WordHistory")
-
 const historyDeleteButton = document.getElementById("delete-history")
-
+// A list of words for the random word function
 const allWords = ['Unique', 'Cacophony', 'Aurora', 'Wonky', 'Elixir', 'Labyrinth', 'Idyllic', 'Melancholy', 'Oblivion', 'Paradox']
-
+// keep a list of words in the history and saved lists
 const savedWordsArray = []
 const historyWordsArray = []
+// Specify a limit for the length of the history section
 const historyLimit = 12
 
-// When the page loads, display a random word
+// When the page loads, display a random word, add it to history, and record it in db.json
 const randomIndex = Math.floor(Math.random() * allWords.length);
 const randomWord = allWords[randomIndex]
 fetchAndDisplay(randomWord)
-// persistHistoryWord(randomWord)
 
-// When the page loads, populate previously saved words from the local databse
+// When the page loads, populate previously saved words from db.json
 fetch(savedWordsUrl)
     .then(res => res.json())
     .then(res => {
@@ -51,7 +40,7 @@ fetch(savedWordsUrl)
         })
     })
 
-
+// When the page loads, populates word history from db.json
 fetch(historyUrl)
     .then(res => res.json())
     .then(res => {
@@ -60,33 +49,16 @@ fetch(historyUrl)
         })
     })
 
-historyDeleteButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    // console.log('i was clicked')
-    deleteHistory()
-    // deleteHistoryLis()
-})
-
 // Search bar submit event listner
 search.addEventListener('submit', (e) => {
     e.preventDefault()
-
     // Defines the searched word
     let word1 = input.value
-
     // uppercase the first letter of the word
     correctCase = word1.charAt(0).toUpperCase() + word1.slice(1);
-
-    fetchAndDisplay(correctCase)
-
+    fetchAndDisplay(correctCase) // populates the searched word, definition, pronunciation, adds word to the history, and records the word in db.json
     // Resets the search bar
     input.value = ''
-})
-
-// Add event listner to the save word button
-saveButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    postSavedWord(wordElement.children[0].textContent)
 })
 
 // Add event listener to the random word button
@@ -97,14 +69,36 @@ randomButton.addEventListener('click', (e) => {
     persistHistoryWord(randomWord)
 })
 
-// Function for rendering Definition to the Definition: Section
+// Make the delete history button work
+historyDeleteButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    deleteHistory() // deletes all word history from db.json and from the page
+})
+
+// Make the saved word button work
+saveButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    postSavedWord(wordElement.children[0].textContent) // Write the saved word to the database
+})
+
+// Take a word and put it in the word node
+function renderWord(newWord) {
+    wordElement.innerHTML = ''
+    const wordTag = document.createElement('p')
+    wordTag.innerText = newWord
+    wordElement.appendChild(wordTag)
+    // If there was an error message, remove it
+    document.getElementById("error-message").textContent = ''
+}
+
+// Take the API's deifinition and put in the definition node
 function renderDefinition(newDef) {
     const pTag = document.createElement('p')
     pTag.innerText = newDef
     definition.appendChild(pTag)
 }
 
-// Function for rendering Pronuciation to the Pronuciation: Section
+// Take the API's pronunciation and put it in the pronunciation node
 function renderPronunciation(newPro) {
     pronunciation.innerHTML = ''
     const pTag = document.createElement('p')
@@ -112,42 +106,42 @@ function renderPronunciation(newPro) {
     pronunciation.appendChild(pTag)
 }
 
-// Shows the word in the definition card
-function renderWord(newWord) {
-    wordElement.innerHTML = ''
-    const wordTag = document.createElement('p')
-    wordTag.innerText = newWord
-    wordElement.appendChild(wordTag)
-    document.getElementById("error-message").textContent = ''
-}
-
 // Each word on the saved list needs a delete button
+// This function gets called by the save button
 function addDeleteButton(word) {
     const deleteButton = document.createElement('button')
     deleteButton.textContent = "Delete"
     deleteButton.classList.add("delete")
     deleteButton.addEventListener('click', (e) => {
         e.preventDefault()
+        // get rid of the saved word's DOM node 
         word.remove()
+        // search for the saved word in db.json and get its id in order to delete it
         fetch(savedWordsUrl)
             .then(res => res.json())
             .then(res => {
+                // need a variable for the text content of the selected word
                 const searchWord = word.querySelector('p').textContent
                 for (let i = 0; i < res.length; i++) {
+                    // find a matching word in db.json/s
                     if (res[i].word === searchWord) {
+                        // when a match is found in the database, retrieve its id
                         index = res[i].id;
+                        // now delete from db.json
                         deleteSavedWord(index)
                         break; // Break the loop if the value is found
                     }
                 }
             })
     })
+    // add the delete button to to the DOM
     word.appendChild(deleteButton)
 }
 
 // Returns true if a word is already saved, false otherwise
 function checkIfAlreadyListed(word, arr) {
     let checkValue = false
+    // iterate through the list of words
     arr.forEach((savedWordsArrayItem) => {
         if (word.trim().toUpperCase() === savedWordsArrayItem.trim().toUpperCase()) {
             checkValue = true;
@@ -157,23 +151,29 @@ function checkIfAlreadyListed(word, arr) {
     return checkValue
 }
 
+// When the 'save word' button gets clicked, save the word
 function renderSavedWord(word) {
+    // record it in our array
     savedWordsArray.push(word)
+    // Add it to the DOM
     const savedWord = document.createElement('li')
     const savedWordp = document.createElement('p')
     savedWordp.innerText = word
     savedWordp.classList.add("saved-word")
+    // Clicking on a saved word should display it again
     savedWordp.addEventListener('click', (e) => {
         e.preventDefault()
-
         fetchAndDisplay(word)
     })
+    // Add event listener to change the color of the word
     colorChange(savedWordp)
     savedWord.appendChild(savedWordp)
+    // give the saved word a delete button
     addDeleteButton(savedWord)
     savedUl.appendChild(savedWord)
 }
 
+// When a word is saved, write it to db.json
 function postSavedWord(wordSaved) {
     if (!(checkIfAlreadyListed(wordSaved, savedWordsArray))) {
         renderSavedWord(wordSaved)
@@ -181,7 +181,7 @@ function postSavedWord(wordSaved) {
     }
 }
 
-// Words should turn green on mouseover, then turn back to their previous color on mouseout
+// Add Event Listeners. Words should turn green on mouseover, then turn back to their previous color on mouseout
 function colorChange(element) {
     let initialColor = element.style.color
     element.addEventListener("mouseover", (e) => {
@@ -194,108 +194,96 @@ function colorChange(element) {
     })
 }
 
-// Add a word to the history list. Called by 
+// Add a word to the history list
 function addToWordHistory(historyWord) {
+    // first make sure it isn't already in the history
     if (!(checkIfAlreadyListed(historyWord, historyWordsArray))) {
+        // record it in the history array
         historyWordsArray.unshift(historyWord)
+        // Create a DOM node for the new entry in the history
         const wordHistory = document.createElement('li')
         wordHistory.innerText = historyWord
-        // Add event listner to the element
+        // Click on the word to display it again
         wordHistory.addEventListener('click', (e) => {
             e.preventDefault()
             fetchAndDisplay(wordHistory.textContent)
         })
+        // MAke the word change color on mouseover
         colorChange(wordHistory)
-        // Appends the word to the history section
+        // Appends the word to the top of the history section
         history.insertBefore(wordHistory, history.firstChild);
-        // while a 13th history node exists, remove it
+        // if the history list is excessively long, shorten it
         while (history.querySelectorAll('li')[historyLimit]) {
             history.querySelectorAll('li')[historyLimit].remove()
         }
-        // Remove all elements in the array with indices greater than the history limit
         historyWordsArray.length = historyLimit
     }
 }
 
+// Remove all the <li> tags in the history section 
 function deleteHistoryLis() {
     const entireHistory = history.querySelectorAll('li')
-    console.log(entireHistory)
     entireHistory.forEach((element) => {
-        console.log(element)
         element.remove()
     })
+    // make our history array empty
     historyWordsArray.length = 0
-    console.log(`history array length is ${historyWordsArray.length}`)
 }
 
 // Stretch goal: add a delete history button that wipes the history in the db
+// The delete button will call this function
 function deleteHistory() {
     deleteHistoryLis()
     fetch(historyUrl)
         .then(res => res.json())
         .then(res => {
-            // const searchWord = word.querySelector('li').textContent
-            console.log(`history db length is ${res.length}`)
-            // debugger
+            // for all entries in the db
             for (let i = 0; i < res.length; i++) {
-                if (true) {
-                    index = res[i].id;
-                    console.log(index)
-                    // debugger
-                    deleteWordHistory(index)
-                    // debugger
-                }
-
+                // get the entry's id
+                index = res[i].id;
+                // fetch delete the id
+                deleteWordHistory(index)
             }
         })
-
 }
 
-
-
-
-
-
-
+// gets a word object from the dictionary and process it
 function fetchAndDisplay(theWord) {
     let wordIsInTheDictionary = false
     let url = `${api}${theWord}${key}`
     fetch(url)
         .then(res => res.json())
         .then(data => {
+            // check if the word is in the dictionary
             if (data[0].shortdef) {
                 wordIsInTheDictionary = true
                 // Accesses Saved Definitions
                 const definitions = data[0].shortdef
-
                 // Accesses Saved Pronuciation
                 const pronounced = data[0].hwi.hw
-
-                // Renders saved pronuciation to the Pronuciation: section
+                // Renders pronuciation to the Pronuciation: section
                 renderPronunciation(pronounced)
-
                 // Resets resets definition
                 definition.innerHTML = ''
-
+                // show the word in the definition section
                 renderWord(theWord)
                 // Renders all definitions
                 definitions.forEach(element => {
                     renderDefinition(element)
                 })
+                // Add to the word history
                 addToWordHistory(theWord)
+                // Add the word to the history section of the db.json
                 persistHistoryWord(theWord)
             }
+            // If the word is not in the dictionary
             else {
                 errorMessage.textContent = `${theWord} is not in the dictionary. Try another word.`
             }
         })
 }
 
-
-// Make the saved words list persist locally by posting them to db.json
-// This function should be called when the 'save word' button is clicked
-
-
+// When the 'saved word' button is clicked, enter the word to db.json
 function persistSavedWord(word) {
     fetch(savedWordsUrl, {
         method: 'POST',
@@ -307,8 +295,7 @@ function persistSavedWord(word) {
     })
 }
 
-
-
+// When a word is added to history, enter it in db.json
 function persistHistoryWord(word) {
     fetch(historyUrl, {
         method: 'POST',
@@ -320,6 +307,7 @@ function persistHistoryWord(word) {
     })
 }
 
+// When the delete button on a saved word is clicked, delete the word from db.json
 function deleteSavedWord(word) {
     fetch(`${savedWordsUrl}/${word}`, {
         method: 'DELETE',
@@ -330,8 +318,9 @@ function deleteSavedWord(word) {
     })
 }
 
+// When the 'delete history' button is clicked, clear out the history section of db.json
+// This gets called in a loop
 function deleteWordHistory(word) {
-    // debugger
     fetch(`${historyUrl}/${word}`, {
         method: 'DELETE',
         headers: {
